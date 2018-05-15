a2338469310a45e1e0af888576ca166270679c8f
psharif <psharif2788@gmail.com>
2018-05-14 18:31:01 -0700

    Added INteger Stuff

---
+++ Interview-Practice-Questions/IntegerQuestions/ReverseInt/reverseInt.js
@@ -0,0 +1,14 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+function reverseInt(n) {}
+
+console.log(`Reverse of 15 is ${reverseInt(15)}`);
+console.log(`Reverse of 981 is ${reverseInt(189)}`);
