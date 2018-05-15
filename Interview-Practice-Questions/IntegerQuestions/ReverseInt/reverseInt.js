196d4d8bb31f70db7c8a994fd55844c731bddb8b
psharif <psharif2788@gmail.com>
2018-05-14 18:35:43 -0700

    Added Integer Method

--- Interview-Practice-Questions/IntegerQuestions/ReverseInt/reverseInt.js
+++ Interview-Practice-Questions/IntegerQuestions/ReverseInt/reverseInt.js
@@ -8,7 +8,14 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    //Turns number into a string
+    let str = n + '';
+    //Split the string into an array.
+    let arr = str.split("");
+    //Reverse the array. Joining it back into a s
+    return parseInt(arr.reverse().join("")) * Math.sign(n);
+}
 
 console.log(`Reverse of 15 is ${reverseInt(15)}`);
 console.log(`Reverse of 981 is ${reverseInt(189)}`);
