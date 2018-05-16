accd5b9417bf65b1dc7ecef287f2eea0e2f627ef
NI GUO <guoni0909@gmail.com>
2018-05-15 20:45:06 -0700

    added comments and README

--- Interview-Practice-Questions/IntegerQuestions/FizzBuzz/FizzBuzz.js
+++ Interview-Practice-Questions/IntegerQuestions/FizzBuzz/FizzBuzz.js
@@ -1,18 +1,23 @@
+
 function fizzBuzz (num) {
+
     for (let i=1; i <= num; i++) {
-        if (i%3 === 0 && i%5 === 0) {
-            console.log("FizzBuzz");
+        //This is the first condition you have to check
+            if (i%3 === 0 && i%5 === 0) {
+                console.log("FizzBuzz");
+            }
+            //If you put this one or any other conditions to line 6, the code will just run this condiction
+            //Which it won't check the line 6
+            else if (i%3 === 0) {
+                console.log("Fizz");
+            }
+            else if (i%5 === 0) {
+                console.log("Buzz");
+            }
+            else {
+                console.log(i);
+            }
         }
-        else if (i%3 === 0) {
-            console.log("Fizz");
-        }
-        else if (i%5 === 0) {
-            console.log("Buzz");
-        }
-        else {
-            console.log(i);
-        }
-    }
 }
 
 fizzBuzz(16);
