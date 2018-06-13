e73b9567736a4e55e334a92ebe8925e9d3ca6cc6
psharif <psharif2788@gmail.com>
2018-06-12 17:04:29 -0700

    Added Comments for circular.js

--- Interview-Practice-Questions/LinkedLists/Circular_Lists/circularList.js
+++ Interview-Practice-Questions/LinkedLists/Circular_Lists/circularList.js
@@ -16,19 +16,24 @@ const Node = L.Node;
 //   c.next = b;
 //   circular(l) // true
 
+///Checks if a LinkedList is ciruclar meaning the tail does not point to null.
 function circular(list) {
+/// Create two references to the first node.
 let fast = list.head;
 let slow = list.head;
-
+  /// While the fast reference has two nodes in front of it.
   while( fast.next && fast.next.next ){
+    /// Increment the slow reference by 1. 
     slow = slow.next;
+    /// Increment the fast reference by 2.
     fast = fast.next.next;
-
+    /// If the two references point to the same node.
     if( fast === slow ){
+      /// Then the list is circular return true.
       return true;
     }
   }
-
+  /// If one of the nodes is pointing to null the return false.
   return false;
 }
 
